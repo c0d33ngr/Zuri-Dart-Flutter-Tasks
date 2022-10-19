b4f6d280889163155e7324e44276e7ed99b8fabc
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-10-19 23:57:33 +0100

    Replace all double datatypes to num datatypes

--- calculator.dart
+++ calculator.dart
@@ -1,40 +1,40 @@
 /// Program to implement a simple calculator
 
 /// Function to add firstNum to secondNum
-double addNumber(double firstNum, double secondNum) {
-	double result = firstNum + secondNum;
+num addNumber(num firstNum, num secondNum) {
+	num result = firstNum + secondNum;
 	return result;
 }
 
 /// Function to substract secondNum value from firsNum
-double substractNumber(double firstNum, double secondNum) {
-	double result = firstNum - secondNum;
+num substractNumber(num firstNum, num secondNum) {
+	num result = firstNum - secondNum;
 	return result;
 }
 
 /// Function to multiply firstNum with secondNum
-double multiplyNumber(double firstNum, double secondNum) {
-	double result = firstNum * secondNum;
+num multiplyNumber(num firstNum, num secondNum) {
+	num result = firstNum * secondNum;
 	return result;
 }
 
 /// Function to divide secondNum from firstNum
-double divideNumber(double firstNum, double secondNum) {
-	double result = firstNum / secondNum;
+num divideNumber(num firstNum, num secondNum) {
+	num result = firstNum / secondNum;
 	return result;
 }
 
 /// Function to get the remainder from the division of firstNum by secondNum with the modulus operator
-double modulusNumber(double firstNum, double secondNum) {
-	double result = firstNum % secondNum;
+num modulusNumber(num firstNum, num secondNum) {
+	num result = firstNum % secondNum;
 	return result;
 }
 
 void main() {
-	double num1 = 22.0;
-	double num2 = 12.0;
+	num num1 = 22.0;
+	num num2 = 12.0;
 	
-	double sumResult, substractResult, multiplyResult, dividedResult, modulusResult;	// list of variables to store the results of each operations performed
+	num sumResult, substractResult, multiplyResult, dividedResult, modulusResult;	// list of variables to store the results of each operations performed
 	
 	sumResult = addNumber(num1, num2);
 	print("$sumResult");
